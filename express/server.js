1c2067c466761bd2b0985f03845419ef27cd8d11
Vivian San <viviansan@Vivians-MacBook-Air.local>
2018-03-08 11:42:14 -0800

    Added nodemailer

--- express/server.js
+++ express/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
 var path = require('path');
 var expressValidator = require('express-validator');
 
@@ -38,18 +39,56 @@ server.post('/url', function(req, res){
       errors: errors
     })
   } else {
-    var newUser = {
-      name: req.body.name,
-      email: req.body.email
-    }
-    console.log(newUser);
+    // var newUser = {
+    //   name: req.body.name,
+    //   email: req.body.email
+    // }
+  //   // console.log(newUser);
+    // console.log(req.body);
+    const output = `
+           <p>Here's the link to the resources doc you requested.<p>
+           <a href="https://docs.google.com/document/d/1XDDsqAiT0WRTMoESiidBEgR_niBPyZJWWudoSTpJRtc/edit?usp=sharing">Resource Link</a>
+           `;
   }
 
   // var email = req.body.email;
   //
   // res.send();
+
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com", // hostname
+    port: 587, // port for secure SMTP
+    secure: false,
+    auth: {
+        user: "fake23480@gmail.com",
+        pass: "fakegmail"
+    },
+    tls:{
+      rejectUnauthorized: false
+    }
+});
+
+// setup e-mail data with unicode symbols
+let mailOptions = {
+    from: "Git Ladies  ✔ <fake23480@gmail.com>", // sender address
+    to: req.body.email, // list of receivers
+    subject: "More Resources WOOO", // Subject line
+    text: "Hello World?",
+    html: output // html body
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        res.render('index');
+
+      });
 });
 
 server.listen(3000, function(){
   console.log('Server Started on Port 3000....')
-})
+});
