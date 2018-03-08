85dcb41d665f54d2dba8fd18ed359acc232e7116
TSL_1 <tsl_1@Techtonicas-MacBook-Air-3.local>
2018-03-07 16:36:00 -0800

    adding

--- express/server.js
+++ express/server.js
@@ -5,6 +5,8 @@ var expressValidator = require('express-validator');
 
 var server = express();
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 //View Engine
 server.set('view engine', 'ejs');
 server.set('views', path.join(__dirname, 'views'));
@@ -12,9 +14,15 @@ server.set('views', path.join(__dirname, 'views'));
 //Express Validator Middleware
 server.use(expressValidator());
 
-server.get('/', function(req, res) {
+server.get('/url', function(req, res) {
   res.render('index');
 });
+
+server.post('/url', urlencodedParser, function(req, res) {
+  console.log("req.formdata");
+  res.render('index');
+});
+
 
 // server.post('/url', function(req, res){
 //   console.log(req.body.email);
